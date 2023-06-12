41a01c8b7bae61f8ffdedc01a2acdb35c45ea0e4
Indira <nizamutdinovaindira@gmail.com>
2023-06-13 01:30:18 +0300

    added capitalize_first_letter def

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def make_capital_letters(word):
+    '''Возвращает слова со всеми заглавными буквами'''
     return word.uppers()
+
+def capitalize_first_letter(word):
+    '''Возвращает слова с первой заглавной буквой'''
+    capitalized_words = []
+    for word in word.split():
+        capitalized_words.append(word.capitalize())
+    return ' '.join(capitalized_words)
+
